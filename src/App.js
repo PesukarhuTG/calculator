263c052ffb10ae39d4a71a0b37b0710d40205968
Tatiana <ganzhinovat@yandex.ru>
2020-08-24 13:13:30 +0300

    feat: task 3 - create logic of calculating expenses, income and balance

--- src/App.js
+++ src/App.js
@@ -10,40 +10,65 @@ class App extends Component {
       transactions: [],
       description: '',
       amount: '',
-    }
+      resultIncome: 0,
+      resultExpenses: 0,
+      totalBalance: 0,
+  }
 
   addTransaction = add => {
 
     //создадим копию массива с помощью спред оператор
-    const transactions = [...this.state.transactions];
-     
-    const transaction = {
-       id: `cmr${(+new Date).toString(16)}`, //быстро генерируем уникальный id
+    const transactions = [...this.state.transactions,
+    {
+       id: `cmr${(+new Date()).toString(16)}`, //быстро генерируем уникальный id
        description: this.state.description,
        amount: this.state.amount,
        add //сюда попадает true / false
-    };
-
-    //теперь можно в копию массива добавить объект, ктр-й создали
-    transactions.push(transaction);
+    }
+  ];
 
     //и направим его в state
     this.setState({
       transactions,
       description: '', //очищаем поле после ввода
-      amount: '',
-    });
-     
+      amount: '',      //очищаем поле после ввода
+    }, this.getTotalBalance);
   }
   
   /* описываем методы для передачи в Operation. 
-  На входе принимает e=элемент, на ктрм сработало событие */
+  На входе принимает e=элемент, на ктрм сработало событие.
+  ParseFloat - чтобы фиксировать число, а не строку */
   addAmount = e => {
-    this.setState({ amount: e.target.value });
+    this.setState({ amount: parseFloat(e.target.value)});
   }
 
   addDescription = e => {
     this.setState({ description: e.target.value });
+  }
+
+  //ф-ция учета Доходов
+  getIncome = () => this.state.transactions
+    .reduce((acc, item) => item.add ? item.amount + acc : acc, 0)
+      //перебираем все транзакции, фильтруя прибыль (у кого add=true)
+      // reduce собирает данные (при этом начальный 0 обязателен) 
+  
+  //ф-ция учета Расходов
+  getExpenses = () =>  this.state.transactions
+    .reduce((acc, item) => !item.add ? item.amount + acc : acc, 0)
+
+  //ф-ция итогового подсчета
+  getTotalBalance() {
+   const resultIncome = this.getIncome();
+   const resultExpenses = this.getExpenses();
+
+   const totalBalance = resultIncome - resultExpenses;
+   
+   //добавляем полученные результаты в setState
+   this.setState({
+    resultIncome,
+    resultExpenses,
+    totalBalance,
+   });
   }
   
   render() {
@@ -56,7 +81,11 @@ class App extends Component {
   
         <main>
           <div className="container">
-            <Total />
+            <Total 
+              resultExpenses={this.state.resultExpenses}
+              resultIncome={this.state.resultIncome}
+              totalBalance={this.state.totalBalance}
+              />
             <History transactions={this.state.transactions} />
             <Operation 
               addTransaction={this.addTransaction} 
